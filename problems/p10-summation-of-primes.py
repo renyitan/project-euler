a825bbe752505d57ea47b51cc99b34cf997cd1ac
Renyi Tan <tanrenyi@gmail.com>
2022-05-09 11:45:53 +0800

    p10

---
+++ problems/p10-summation-of-primes.py
@@ -0,0 +1,26 @@
+def run():
+    n = 2000000
+    # use of sieve of erathosthenes to find prime in range(1,n+1)
+    return sieve_and_sum(n)
+
+
+def sieve_and_sum(n):
+    prime = [True for _ in range(n+1)]
+    p = 2
+
+    while (p*p <= n):
+        # if prime[p] is not changed, it is a prime
+
+        if prime[p]:
+
+            # update all multiples of p
+            for i in range(p**2, n+1, p):
+                prime[i] = False
+        p += 1
+    prime[0] = False
+    prime[1] = False
+    return sum([i for i, is_prime in enumerate(prime) if is_prime and i >= 2])
+
+
+if __name__ == "__main__":
+    print(run())
