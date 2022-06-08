f7d9802210671f141b18e873584f9a5fae897190
Renyi Tan <tanrenyi@gmail.com>
2022-06-08 16:02:08 +0800

    p42

--- problems/p42-coded-triangle-numbers.py
+++ problems/p42-coded-triangle-numbers.py
@@ -17,30 +17,22 @@ def is_triangular(n):
     if (n < 0):
         return False
 
-    # Considering the equation n*(n+1)/2 = num
-    # The equation is : a(n^2) + bn + c = 0
-    c = (-2 * n)
-    b, a = 1, 1
+    c,b,a = (-2 * n), 1,1
     d = (b * b) - (4 * a * c)
 
     if (d < 0):
         return False
 
-    # Find roots of equation
     root1 = (-b + math.sqrt(d)) / (2 * a)
     root2 = (-b - math.sqrt(d)) / (2 * a)
 
-    # checking if root1 is natural
     if (root1 > 0 and math.floor(root1) == root1):
         return True
 
-    # checking if root2 is natural
     if (root2 > 0 and math.floor(root2) == root2):
         return True
 
     return False
-
-
 
 def run():
     with open('p042_words.txt') as f:
